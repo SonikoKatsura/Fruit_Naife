5190132234f56f9b4cd840321f489a3354543973
Soniko <sonikonyas@gmail.com>
2024-05-09 18:26:45 +0100

    Barrel & Spline

--- Assets/Scripts/Spline.cs
+++ Assets/Scripts/Spline.cs
@@ -1,13 +1,10 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
 
 [ExecuteInEditMode]
 public class Spline : MonoBehaviour
 {
-    [SerializeField]
-    private Transform _start, _middle, _end;
+    public Transform _start, _middle, _end;
 
     [SerializeField]
     private bool showGizmos = true;
@@ -18,19 +15,37 @@ public class Spline : MonoBehaviour
     [SerializeField, Range(0.25f, 0.75f)]
     private float _placementOffset = 0.5f;
 
+    [SerializeField]
+    private GameObject objectPrefab;
+
+    [SerializeField]
+    private Transform player;
+
+    [SerializeField]
+    private float launchRadius = 2f;
+
 #if UNITY_EDITOR
     private void Update()
     {
-        Vector3 start = _start.position;
-        Vector3 end = _end.position;
-        Vector3 midPointPosition = Vector3.Lerp(start, end, _placementOffset);
-        midPointPosition.y += _heightOffset;
-        SetPoints(
-            start,
-            midPointPosition,
-            end);
+        Vector3 startPoint = _start.position;
+        Vector3 targetPosition = player.position + Random.insideUnitSphere * launchRadius;
+        CalculateMidPoint(startPoint, targetPosition);
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            LaunchObject();
+        }
     }
 #endif
+
+    public void CalculateMidPoint(Vector3 startPoint, Vector3 targetPosition)
+    {
+        Vector3 end = targetPosition;
+        Vector3 midPointPosition = Vector3.Lerp(startPoint, end, _placementOffset);
+        midPointPosition.y += _heightOffset;
+        SetPoints(startPoint, midPointPosition, end);
+    }
 
     private Vector3 CalculatePosition(float value01, Vector3 startPos,
         Vector3 endPos, Vector3 midPos)
@@ -47,12 +62,12 @@ public class Spline : MonoBehaviour
 
     public Vector3 CalculatePositionCustomStart(float interpolationAmount01,
         Vector3 startPosition)
-    => CalculatePosition(interpolationAmount01,
+        => CalculatePosition(interpolationAmount01,
             startPosition, _end.position, _middle.position);
 
     public Vector3 CalculatePositionCustomEnd(float interpolationAmount01,
         Vector3 endPosition)
-    => CalculatePosition(interpolationAmount01,
+        => CalculatePosition(interpolationAmount01,
             _start.position, endPosition, _middle.position);
 
     public void SetPoints(Vector3 startPoint, Vector3 midPointPosition,
@@ -65,6 +80,32 @@ public class Spline : MonoBehaviour
             _end.position = endPoint;
         }
     }
+
+    private void LaunchObject()
+    {
+        Vector3 startPoint = _start.position;
+        Vector3 targetPosition = player.position + Random.insideUnitSphere * launchRadius;
+        CalculateMidPoint(startPoint, targetPosition);
+
+        GameObject newObject = Instantiate(objectPrefab, startPoint, Quaternion.identity);
+        StartCoroutine(LaunchObjectCoroutine(newObject, startPoint, targetPosition));
+    }
+
+    private IEnumerator LaunchObjectCoroutine(GameObject objectToLaunch, Vector3 startPoint, Vector3 endPoint)
+    {
+        float duration = Vector3.Distance(startPoint, endPoint);
+        float t = 0f;
+
+        while (t < 1f)
+        {
+            t += Time.deltaTime / duration;
+            objectToLaunch.transform.position = CalculatePosition(t, startPoint, endPoint, _middle.position);
+            yield return null;
+        }
+
+        Destroy(objectToLaunch);
+    }
+
     private void OnDrawGizmos()
     {
         if (showGizmos && _start != null && _middle != null && _end != null)
@@ -88,5 +129,4 @@ public class Spline : MonoBehaviour
 
         }
     }
-
 }
