b8f08542a13fe08b9fe0d83f515beb0d672ebb76
Armas <rubenarmas2010@gmail.com>
2024-05-16 20:38:00 +0100

    lives ui hide/show sprites

--- Assets/Scripts/UI/InGameUI.cs
+++ Assets/Scripts/UI/InGameUI.cs
@@ -8,7 +8,12 @@ public class InGameUI : MonoBehaviour {
     [SerializeField] GameManager gameManager;
 
     [SerializeField] TextMeshProUGUI pointsTxt;
-    [SerializeField] TextMeshProUGUI livesTxt;
+
+    [Header("Lives")]
+    //[SerializeField] TextMeshProUGUI livesTxt;
+    [SerializeField] GameObject Heart_1;
+    [SerializeField] GameObject Heart_2;
+    [SerializeField] GameObject Heart_3;
 
     void Start() {
         if (gameManager == null) {
@@ -20,6 +25,30 @@ public class InGameUI : MonoBehaviour {
 
     void Update() {
         pointsTxt.text = gameManager.GetPoints().ToString();
-        livesTxt.text = gameManager.GetLives().ToString();
+        HideShowHearts();
+        //livesTxt.text = gameManager.GetLives().ToString();
+    }
+
+    private void HideShowHearts() {
+        int lives = gameManager.GetLives();
+        switch (lives) {
+            case 3:
+                Heart_1.SetActive(true);
+                Heart_2.SetActive(true);
+                Heart_3.SetActive(true);
+                break;
+            case 2:
+                Heart_3.SetActive(false);
+                break;
+            case 1:
+                Heart_2.SetActive(false);
+                break;
+            case 0:
+                Heart_1.SetActive(false);
+                break;
+            default:
+                // code block
+                break;
+        }
     }
 }
