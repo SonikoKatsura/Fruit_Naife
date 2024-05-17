941544a8835b52b98308235190cabdb8ab228a89
Rinosauruss <166913268+Rinosauruss@users.noreply.github.com>
2024-05-17 19:52:11 +0100

    music settings

--- Assets/Scripts/Music ValueChange.cs
+++ Assets/Scripts/Music ValueChange.cs
@@ -2,22 +2,28 @@ using Oculus.Interaction.Editor;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Volume : MonoBehaviour
 {
 
-    private AudioSource audioSRC;
+    public AudioSource AudioSource;
     private float musicvolume = 1f;
+    public Slider volumeSlider;
 
     void Start()
     {
-       audioSRC  = GetComponent<AudioSource>(); 
+       AudioSource.Play();
+        musicvolume = PlayerPrefs.GetFloat("volume");
+        AudioSource.volume = musicvolume;
+        volumeSlider.value = musicvolume;  
     }
 
     // Update is called once per frame
     void Update()
     {
-        audioSRC.volume = musicvolume;
+        AudioSource.volume = musicvolume;
+        PlayerPrefs.SetFloat("volume", musicvolume);
     }
     public void SetVolume(float volume)
     {
